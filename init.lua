92b54e8bfb717bda47d6f90af2b6607f3d1a06b4
CakeVision <andreicodorean1811@gmail.com>
2024-10-04 13:20:22 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.clipboard = 'unnamedplus'
